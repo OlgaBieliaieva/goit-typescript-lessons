1fc175cb387bf5e57e95c2a6b7c3b603d6c5e52e
Olga Bieliaieva <belyaevaolya0@gmail.com>
2023-03-21 08:37:43 +0200

    some exercises

---
+++ app.js
@@ -0,0 +1,20 @@
+// const button = document.querySelector("button")! as HTMLButtonElement;
+// const input1 = document.getElementById("num1")! as HTMLInputElement;
+// const input2 = document.getElementById("num2")! as HTMLInputElement;
+// function add(num1: number, num2: number) {
+//   return num1 + num2;
+// }
+// button.addEventListener("click", function () {
+//   console.log(add(+input1.value, +input2.value));
+// });
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["USER"] = 1] = "USER";
+})(Role || (Role = {}));
+var person = {
+    role: Role.ADMIN
+};
+if (person.role === Role.ADMIN) {
+    console.log("Role: ", Role.ADMIN);
+}
---
+++ app.ts
@@ -0,0 +1,24 @@
+// const button = document.querySelector("button")! as HTMLButtonElement;
+// const input1 = document.getElementById("num1")! as HTMLInputElement;
+// const input2 = document.getElementById("num2")! as HTMLInputElement;
+
+// function add(num1: number, num2: number) {
+//   return num1 + num2;
+// }
+
+// button.addEventListener("click", function () {
+//   console.log(add(+input1.value, +input2.value));
+// });
+
+enum Role {
+  ADMIN,
+  USER,
+}
+
+const person = {
+  role: Role.ADMIN,
+};
+
+if (person.role === Role.ADMIN) {
+  console.log("Role: ", Role.ADMIN);
+}
